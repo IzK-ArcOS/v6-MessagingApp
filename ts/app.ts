f4e93d17f713584833bf8574ac89192bddff3a46
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-02-13 09:38:13 +0100

    Set metadata

--- ts/app.ts
+++ ts/app.ts
@@ -3,31 +3,31 @@ import { Runtime } from "./runtime";
 import AppSvelte from "../App.svelte";
 import { App } from "$types/app";
 
-export const app: App = {
+export const MessagingApp: App = {
   metadata: {
-    name: "App Template",
+    name: "Messages",
     description: "This is an app template",
     author: "The ArcOS Team",
-    version: "0.0.0",
+    version: "2.0.0",
     icon: DefaultIcon
   },
   runtime: Runtime,
   content: AppSvelte,
-  id: "appTemplate",
-  size: { w: 0, h: 0 },
-  minSize: { w: 0, h: 0 },
-  maxSize: { w: 0, h: 0 },
-  pos: { x: 0, y: 0 },
+  id: "MessagingApp",
+  size: { w: 600, h: 450 },
+  minSize: { w: 500, h: 300 },
+  maxSize: { w: 1800, h: 1000 },
+  pos: { x: 100, y: 100 },
   state: {
     minimized: false,
     maximized: false,
     headless: false,
     fullscreen: false,
-    resizable: false
+    resizable: true
   },
   controls: {
-    minimize: false,
-    maximize: false,
-    close: false
+    minimize: true,
+    maximize: true,
+    close: true
   }
 }
